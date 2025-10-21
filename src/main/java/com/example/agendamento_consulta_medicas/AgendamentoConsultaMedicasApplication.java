3d9a4b9a4e23c81f32f867b2fe56fb94939b7260
Hugo Aragao <hugoarly01@gmail.com>
2025-10-21 20:36:54 -0300

    initial commit

---
+++ src/main/java/com/example/agendamento_consulta_medicas/AgendamentoConsultaMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.example.agendamento_consulta_medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoConsultaMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoConsultaMedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agendamento_consulta_medicas/AgendamentoConsultaMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agendamento_consulta_medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoConsultaMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
